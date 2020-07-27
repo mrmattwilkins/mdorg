dc221927eeeebb38cca5fc3ba1a7aecf33a16cd4
Matt Wilkins <matt.wilkins@niwa.co.nz>
2020-07-27 14:20:10 +1200

    More javascript now, search is ajax

--- ws/core/urls.py
+++ ws/core/urls.py
@@ -5,25 +5,17 @@ from . import views
 app_name = 'core'
 urlpatterns = [
 
+   path('', views.home, name='home'),
    path('rescan/', views.rescan, name='rescan'),
-   path('search/', views.search, name='search'),
+
    #path('search/<str:text>/', views.search, name='search'),
 
-   
+   path('search/', views.search, name='search'),
 
-   path('', views.items, name='items'),
-   path('items/<int:tag>/', views.items, name='items'),
-
-   path('item/<int:pkey>/', views.item, name='item'),
-   path('item/<int:pkey>/<int:tag>', views.item, name='item'),
-
-
-
-
-   path('zitems/', views.zitems, name='zitems'),
-   path('zitem/', views.zitem, name='zitem'),
-   path('zitem/delete/', views.zdelete, name='zitem_delete'),
-   path('ztags/', views.ztags, name='ztags'),
+   path('items/', views.items, name='items'),
+   path('item/', views.item, name='item'),
+   path('item/delete/', views.delete, name='item_delete'),
+   path('tags/', views.tags, name='tags'),
 
    #path('item/<int:pkey>/edit/', item.edit, name='item_edit'),
    #path('item/new/', item.new, name='item_new'),
--- ws/core/views.py
+++ ws/core/views.py
@@ -14,8 +14,23 @@ from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 
-
 from .models import Item, Tag
+
+def home(request, msg=None):
+    """Home view
+
+    Parameters
+    ----------
+    request: django.http.request
+
+    msg: str
+        A mark_safed string to display in the main
+    """
+
+    return render(request, 'core/items.html', {
+        'tags': Tag.objects.all().order_by('name').values_list('name', flat=True),
+        'msg': msg
+    })
 
 def rescan(request):
     """Scan the document repo."""
@@ -35,24 +50,46 @@ def rescan(request):
         except Exception as exp:
             msg.append(str(exp))
    
-    return items(request, msg=mark_safe('<br />'.join(msg)))
+    return home(request, msg=mark_safe('<br />'.join(msg)))
 
+@require_http_methods(["GET"])
 def search(request):
-    """Search for items containing text."""
+    """Search for items containing text.
+
+    Parameters
+    ----------
+    request : django.http.request
+        request['tag'] is a tag name, restrict search to this tag
+        request['lookfor'] string to look for
+
+    Returns
+    -------
+    JsonResponse
+        {
+            'success': bool
+            'items': list [(id, title, context), (id, title, context) ...]
+            'msg': str
+        }
+        The msg is only there if there was a problem
+
+        If tag is 'Recipes' then search in all items.
+
+    """
 
     txt, tag = request.GET['lookfor'], request.GET.get('tag', None)
 
-    if tag:
-        its = Item.objects.filter(tags__id=tag).order_by('title')
+    if tag and tag not in ('Recipes', 'recent'):
+        its = Item.objects.filter(tags__name=tag).order_by('title')
     else:
         its = Item.objects.all().order_by('title')
 
-    msg = ['<ul>']
+    items = []
     for it in its:
         found = None
 
         # found refers to Item, and if we found txt in the body context gives
         # us the string where it was found
+
         if re.search(txt, it.title, re.IGNORECASE):
             found, context = it, None
         else:
@@ -64,102 +101,16 @@ def search(request):
                     context = f"...{body[max(m.start()-20, 0):min(m.end()+20, len(body))]}..."""
        
         if found:
-            args = [found.id, tag] if tag else [found.id]
-            msg.append(f"""<li><a href="{reverse('core:item', args=args)}">{found.title}</a>""")
-            msg.append(f"{'<br />' + context if context else ''}</li>")
+            items.append([it.id, it.title, context])
 
-    msg.append('</ul>')
-
-    return items(request, tag=tag, msg=mark_safe(''.join(msg)))
-
-def items(request, tag=None, msg=None, hideme=None):
-    """Return items in sidebar with possible message in main
-
-    Parameters
-    ----------
-    request: django.http.request
-
-    tag: int
-        Tag id.  If given only these items will be in sidebar (otherwise all)
-        -1 is a special tag that means order by most recently accessed
-    msg: str
-        A mark_safed string to display in the main
-
-    """
-
-    # get all tags to display at the top
-    tags = [
-        (t.id, t.name)
-        for t in Tag.objects.all().order_by('name')
-    ]
-    #tags.append((0, 'recent'))
-
-    if tag:
-        its = Item.objects.filter(tags__id=tag).order_by('title')
-    else:
-        its = Item.objects.all().order_by('title')
-
-    return render(request, 'core/items.html', {
-        'tags': tags,
-        'tag': tag,
-        'items': its,
-        'hideme': hideme,
-        'msg': msg
+    return JsonResponse({
+        'success': True,
+        'items': json.dumps(items)
     })
 
-def item(request, pkey, tag=None):
-    """View of item
-
-    Parameters
-    ----------
-    request: django.http.request
-
-    pkey: int
-        The Item id to display in main
-    tag: int
-        Tag id.  If given only these items will be in sidebar (otherwise all)
-    
-    """
-
-    it = Item.objects.get(pk=pkey)
-
-    msg = []
-    msg.append(f'<h2 style="display:inline;">{it.title}</h2>')
-
-    # tags in () after title
-    if it.tags.all():
-        msg.append(' (')
-        msg.append(', '.join(
-            f"""<a href="{reverse('core:items', args=(t.id,))}">{t.name}</a>"""
-            for t in it.tags.all()
-        ))
-        msg.append(')')
-    msg.append('<br />')
-    
-    if it.created:
-        msg.append(f'<small><i>Created: </i>{timezone.localtime(it.created).strftime("%Y-%m-%d %H:%M")}</small><br />')
-
-    last = timezone.localtime(it.accessed).strftime("%Y-%m-%d %H:%M") if it.accessed else 'never'
-    msg.append(f'<small><i>Last access: </i>{last}</small><br />')
-
-    msg.append(f'<small><i>Filename: </i>{os.path.basename(it.path)}</small><br />')
-    msg.append('<br />')
-
-    # get the content
-    with open(it.path, "r", encoding="utf-8") as fh:
-        text = frontmatter.load(fh)
-        msg.append(markdown.markdown(text.content))
-
-    # save back with correct accessed
-    with open(it.path, "w", encoding="utf-8") as fh:
-        text['accessed'] = it.accessed = timezone.now()
-        fh.write(frontmatter.dumps(text))
-        it.save()
-
-    return items(request, tag, msg=mark_safe(''.join(msg)), hideme='d-none d-sm-block')
 
 @require_http_methods(["GET"])
-def zitems(request):
+def items(request):
     """Returns items in given tag
 
     Parameters
@@ -204,7 +155,7 @@ def zitems(request):
 
 
 @require_http_methods(["GET"])
-def ztags(request):
+def tags(request):
     """The tags
 
     Parameters
@@ -236,7 +187,7 @@ def ztags(request):
 
 
 @require_http_methods(["GET"])
-def zitem(request):
+def item(request):
     """An item
 
     Parameters
@@ -252,7 +203,7 @@ def zitem(request):
             'title': str
             'created': str (YYYY-MM-DD HH:MM:SS) or None
             'last': str either YYYY-MM-DD HH:MM:SS or 'never'
-            'tags': list of tag ids
+            'tags': list of tag names
             'filename': str
             'content': html str
             'msg': str
@@ -266,7 +217,7 @@ def zitem(request):
     except Exception as exp: # pylint: disable=broad-except
         return JsonResponse({'success': False, 'msg': str(exp)})
     
-    tags = [t.id for t in it.tags.all()]
+    tags = [t.name for t in it.tags.all()]
     created = timezone.localtime(it.created).strftime("%Y-%m-%d %H:%M") if it.created else None
     last = timezone.localtime(it.accessed).strftime("%Y-%m-%d %H:%M") if it.accessed else 'never'
 
@@ -295,7 +246,7 @@ def zitem(request):
 
 
 @require_http_methods(["GET"])
-def zdelete(request):
+def delete(request):
     """Delete an item
 
     Parameters
